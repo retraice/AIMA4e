e9615f8ad45a9c70e99935e0622a8de185eeb526
jeffrecode <jeffrecode@gmail.com>
2022-12-20 04:23:18 -0500

    After Re87 Notes.

--- Re87--BEST-FIRST-Part-6/BEST-FIRST-SEARCH-Re87.py
+++ Re87--BEST-FIRST-Part-6/BEST-FIRST-SEARCH-Re87.py
@@ -81,7 +81,7 @@ class RouteProblem(Problem): # e.g. atobproblem = RouteProblem('A', 'B', map=sim
     # Create a problem with RouteProblem(start, goal, map=Map(...)}).              # here we'd pass the map romania
     # States are the vertexes in the Map graph; actions are destination states."""
     
-    def actions(self, state):   # e.g. atobproblem.actions(s1)
+    def actions(self, state):   # e.g. atobproblem.actions('A')
         # """The places neighboring `state`."""
         return self.map.neighbors[state] # actions-STEP-1, e.g. atobproblem.map.neighbors['A']
     
@@ -110,35 +110,83 @@ class Map:
     # If `directed=False` then for every (v1, v2) link, we add a (v2, v1) link."""
 
     def __init__(self, links, locations=None, directed=False):
-        if not hasattr(links, 'items'):                            # Distances are 1 by default --AIMA ## returns t or f
-            links = {link: 1 for link in links}                    # If 
-        if not directed:                                           # 'directed=False' by default above;
-            for (v1, v2) in list(links):                           # list() creates list object.
-                links[v2, v1] = links[v1, v2]                      # 
-        self.distances = links
-        self.neighbors = multimap(links)
+        if not hasattr(links, 'items'):          # Distances are 1 by default --AIMA ## returns t or f
+            links = {link: 1 for link in links}  # If your links doesn't have 'items' ?attributes, set each 'items' to 1
+        if not directed:                         # 'directed=False' by default above;
+            for (v1, v2) in list(links):         # list() creates list object.
+                links[v2, v1] = links[v1, v2]    # This adds mirror-like entries in our dict, for backtracking
+        self.distances = links                   # distances attribute is the updated, backtrack-able links dict.
+        self.neighbors = multimap(links)	 # get all neighbors of all states, i.e. actions available at each state
         self.locations = locations or defaultdict(lambda: (0, 0))  # "from collections import defaultdict" above
 
-def multimap(pairs) -> dict:     # "-> dict" is function annotation https://peps.python.org/pep-3107/
-                                 # "Given (key, val) pairs, make a dict of {key: [val,...]}."
-    result = defaultdict(list)   # "from collections import defaultdict" above; list() creates list object.
-    for key, val in pairs:       # pairs will be links, as in "self.neighbors = multimap(links)" above
-        result[key].append(val)  # add key value pair to defaultdict
+def multimap(pairs) -> dict:    # "-> dict" is function annotation https://peps.python.org/pep-3107/
+                                # "Given (key, val) pairs, make a dict of {key: [val,...]}."
+    result = defaultdict(list)  # "from collections import defaultdict" above; list() creates list object....hmm 
+    for key, val in pairs:	# pairs will be links, as in "self.neighbors = multimap(links)" above
+       result[key].append(val)  # add key value pair to defaultdict 
     return result
+
+# # saving the debugging, verbose version of mutlimap.  Does this print mostly blank dicts because print vs return?
+# def multimap(pairs) -> dict:     # "-> dict" is function annotation https://peps.python.org/pep-3107/
+#                                  # "Given (key, val) pairs, make a dict of {key: [val,...]}."
+#     result = defaultdict(list)   # "from collections import defaultdict" above; list() creates list object.
+#     # print("Before the for loop...")
+#     # print(defaultdict(list))
+#     # i=1
+#     for key, val in pairs:       # pairs will be links, as in "self.neighbors = multimap(links)" above
+#         result[key].append(val)  # add key value pair to defaultdict
+#        #  print(i)
+#        #  print(defaultdict(list))
+#        # i = i+1
+#     return result
 ########################################################################################################################
 # run Re87
 
-# dir(<self>)                 # ?see all methods and properties... No! "list of valid attributes"
+# useful probing tools: 
+# dir(<self>)                 # ?see all methods and properties... No! "list of valid attributes" ... hmmm
                               # https://docs.python.org/3/library/functions.html#dir
-                              
 # <self>.__dict__             # see all attributes and values, "The namespace supporting arbitrary function attributes."
                               # https://docs.python.org/3/reference/datamodel.html
-
-tlinks = {('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# t stands for test                              
+tlinks = {('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118, ('Yo', 'Momma'): 90, ('Momma', 'Dadda'): 91}
 tlinksNoDistances = {('A', 'Z'), ('A', 'S'), ('A', 'T')}
 tlocations = {'A': ( 76, 497), 'S': (187, 463), 'T': ( 83, 414), 'Z': (92, 539)}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# To get symmetrical (reversible, backtrack-able) actions: 
+# tmap instantiation above must be done first, before print(multimap(tlinks)), so that "links[v2, v1] = links[v1, v2]"
+# is executed.  This was the cause of the Re87 bug, caused at YT video 1:03:20.
+# But why do the functions modify tlinks?  Side effects.  A function is not an object.  So it modifies global state. ?
+# And the reason, at 1:05:10, that only the last print of defualtdict has data is because ... print vs. return?
+# print(multimap(tlinks))
+# print(tmap.neighbors)
+# ➜  Re87--BEST-FIRST-Part-6 git:(main) ✗ python BEST-FIRST-SEARCH-Re87.py
+# defaultdict(<class 'list'>, {'A': ['Z', 'S', 'T'], 'Yo': ['Momma'], 'Momma': ['Dadda', 'Yo'], 'Z': ['A'], 'S': ['A'], 'T': ['A'], 'Dadda': ['Momma']})
+# defaultdict(<class 'list'>, {'A': ['Z', 'S', 'T'], 'Yo': ['Momma'], 'Momma': ['Dadda', 'Yo'], 'Z': ['A'], 'S': ['A'], 'T': ['A'], 'Dadda': ['Momma']})
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# We'll use id() to confirm we're changing a specific object (tlinks) by instantiating tmap:
+# https://docs.python.org/3/library/functions.html#id
+# "Return the “identity” of an object. This is an integer which is guaranteed to be unique and constant for this object
+# during its lifetime. Two objects with non-overlapping lifetimes may have the same id() value."
+print(id(tlinks))
+print(tlinks) # original
 tmap = Map (tlinks, tlocations)
-tmapno = Map (tlinksNoDistances, tlocations)
+print(id(tlinks))      
+print(tlinks) # now modified, but why? ... SIDE EFFECTS!!! multimap is a function! So it f's up our original dictionary.
+# Output: 
+# ➜  Re87--BEST-FIRST-Part-6 git:(main) ✗ python BEST-FIRST-SEARCH-Re87.py
+# 4330553152
+# {('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118, ('Yo', 'Momma'): 90, ('Momma', 'Dadda'): 91}
+# 4330553152
+# {('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118, ('Yo', 'Momma'): 90, ('Momma', 'Dadda'): 91, <NEWLINE>
+#  ('Z', 'A'): 75, ('S', 'A'): 140, ('T', 'A'): 118, ('Momma', 'Yo'): 90, ('Dadda', 'Momma'): 91}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# various previous tests: 
+
+# tmapno = Map (tlinksNoDistances, tlocations)
 
 # print("Does tlinks dict. have distances?")
 # print(hasattr(tlinks, 'items'))
@@ -151,34 +199,37 @@ tmapno = Map (tlinksNoDistances, tlocations)
 # print("Does tlinks dict. have items?")
 # print(hasattr(tlinks, 'items'))
 # print(tmap.distances)
+# print(tlinks.__class__)
+# print(dir(tlinks))
+# print("\nThis is a gap.\n")
+# print(dir(tlinksNoDistances))
 
-print(dir(tlinks))
-#print(tlinks.__class__)
+# print(dir(tlinks))
+# print(tlinks.__class__)
 # print(tmap.__dict__)
 # print(dict(tlinksNoDistances))
 # print(dir(tlinksNoDistances))
 # print(dir(tmap))
 
-
-
 ########################################################################################################################
+# AIMA: 
 # Some specific RouteProblems
-romania = Map(  # instantiate class Map with following arguments: 
-    # 'links': acitons (p. 71): "`links` can be either [(v1, v2)...] pairs, or a {(v1, v2): distance...} dict."
-    {('O', 'Z'):  71, ('O', 'S'): 151, ('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118, 
-     ('L', 'T'): 111, ('L', 'M'):  70, ('D', 'M'): 75, ('C', 'D'): 120, ('C', 'R'): 146, 
-     ('C', 'P'): 138, ('R', 'S'):  80, ('F', 'S'): 99, ('B', 'F'): 211, ('B', 'P'): 101, 
-     ('B', 'G'):  90, ('B', 'U'):  85, ('H', 'U'): 98, ('E', 'H'):  86, ('U', 'V'): 142, 
-     ('I', 'V'):  92, ('I', 'N'):  87, ('P', 'R'): 97},
-    # 'locations': state space (p. 71): "Optional `locations` can be {v1: (x, y)}"
-    {'A': ( 76, 497), 'B': (400, 327), 'C': (246, 285), 'D': (160, 296), 'E': (558, 294), 
-     'F': (285, 460), 'G': (368, 257), 'H': (548, 355), 'I': (488, 535), 'L': (162, 379),
-     'M': (160, 343), 'N': (407, 561), 'O': (117, 580), 'P': (311, 372), 'R': (227, 412),
-     'S': (187, 463), 'T': ( 83, 414), 'U': (471, 363), 'V': (535, 473), 'Z': (92, 539)})
+# romania = Map(  # instantiate class Map with following arguments: 
+#     # 'links': acitons (p. 71): "`links` can be either [(v1, v2)...] pairs, or a {(v1, v2): distance...} dict."
+#     {('O', 'Z'):  71, ('O', 'S'): 151, ('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118, 
+#      ('L', 'T'): 111, ('L', 'M'):  70, ('D', 'M'): 75, ('C', 'D'): 120, ('C', 'R'): 146, 
+#      ('C', 'P'): 138, ('R', 'S'):  80, ('F', 'S'): 99, ('B', 'F'): 211, ('B', 'P'): 101, 
+#      ('B', 'G'):  90, ('B', 'U'):  85, ('H', 'U'): 98, ('E', 'H'):  86, ('U', 'V'): 142, 
+#      ('I', 'V'):  92, ('I', 'N'):  87, ('P', 'R'): 97},
+#     # 'locations': states (p. 71): "Optional `locations` can be {v1: (x, y)}"
+#     {'A': ( 76, 497), 'B': (400, 327), 'C': (246, 285), 'D': (160, 296), 'E': (558, 294), 
+#      'F': (285, 460), 'G': (368, 257), 'H': (548, 355), 'I': (488, 535), 'L': (162, 379),
+#      'M': (160, 343), 'N': (407, 561), 'O': (117, 580), 'P': (311, 372), 'R': (227, 412),
+#      'S': (187, 463), 'T': ( 83, 414), 'U': (471, 363), 'V': (535, 473), 'Z': (92, 539)})
 
-r0 = RouteProblem('A', 'A', map=romania)
-r1 = RouteProblem('A', 'B', map=romania)
-r2 = RouteProblem('N', 'L', map=romania)
-r3 = RouteProblem('E', 'T', map=romania)
-r4 = RouteProblem('O', 'M', map=romania)
+# r0 = RouteProblem('A', 'A', map=romania)
+# r1 = RouteProblem('A', 'B', map=romania)
+# r2 = RouteProblem('N', 'L', map=romania)
+# r3 = RouteProblem('E', 'T', map=romania)
+# r4 = RouteProblem('O', 'M', map=romania)
 
